6cb42861df2fa323420cbd3c7629dbcb9c17ee5f
AlexeyTyan <alekseytyan45@gmail.com>
2021-03-31 12:16:04 +0300

    Add test method for BFS and DFS

---
+++ Lab4Alexey/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab4Alexey {
+    class Program {
+        static void Main(string[] args) {
+            // Deep-First Search algorithm
+            Graph g = new Graph(4);
+ 
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 2);
+            g.AddEdge(1, 2);
+            g.AddEdge(2, 0);
+            g.AddEdge(2, 3);
+            g.AddEdge(3, 3);
+ 
+            Console.WriteLine(
+                "Following is Depth First Traversal "
+                + "(starting from vertex 2)");
+ 
+            g.DFS_method(2);
+            Console.ReadKey();
+            
+            // Breadth-First Search algorithm
+            Graph g2 = new Graph(4);
+     
+            g2.AddEdge(0, 1);
+            g2.AddEdge(0, 2);
+            g2.AddEdge(1, 2);
+            g2.AddEdge(2, 0);
+            g2.AddEdge(2, 3);
+            g2.AddEdge(3, 3);
+     
+            Console.Write("Following is Breadth First " +
+                          "Traversal(starting from " +
+                          "vertex 2)\n");
+            g2.BFS_method(2);
+            
+            
+            
+            
+        }
+    }
+}
