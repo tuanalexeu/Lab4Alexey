10c6ad1d8f3db37af48e278676642bbd8e4858b9
AlexeyTyan <alekseytyan45@gmail.com>
2021-03-31 12:31:02 +0300

    Add test for Kruskal's algorithm

--- Lab4Alexey/Program.cs
+++ Lab4Alexey/Program.cs
@@ -14,11 +14,9 @@ namespace Lab4Alexey {
             g.AddEdge(3, 3);
  
             Console.WriteLine(
-                "Following is Depth First Traversal "
-                + "(starting from vertex 2)");
+                "Поиск в глубину:");
  
             g.DFS_method(2);
-            Console.ReadKey();
             
             // Breadth-First Search algorithm
             Graph g2 = new Graph(4);
@@ -30,12 +28,41 @@ namespace Lab4Alexey {
             g2.AddEdge(2, 3);
             g2.AddEdge(3, 3);
      
-            Console.Write("Following is Breadth First " +
-                          "Traversal(starting from " +
-                          "vertex 2)\n");
+            Console.Write("Поиск в ширину:\n");
             g2.BFS_method(2);
-            
-            
+
+            Console.WriteLine("Алгоритм Крускала:");
+            // Kruskal's algorithm
+            int verticesCount = 4;
+            int edgesCount = 5;
+            KruskalAlgorithm.Graph graph = KruskalAlgorithm.CreateGraph(verticesCount, edgesCount);
+
+            // Edge 0-1
+            graph.edge[0].Source = 0;
+            graph.edge[0].Destination = 1;
+            graph.edge[0].Weight = 10;
+
+            // Edge 0-2
+            graph.edge[1].Source = 0;
+            graph.edge[1].Destination = 2;
+            graph.edge[1].Weight = 6;
+
+            // Edge 0-3
+            graph.edge[2].Source = 0;
+            graph.edge[2].Destination = 3;
+            graph.edge[2].Weight = 5;
+
+            // Edge 1-3
+            graph.edge[3].Source = 1;
+            graph.edge[3].Destination = 3;
+            graph.edge[3].Weight = 15;
+
+            // Edge 2-3
+            graph.edge[4].Source = 2;
+            graph.edge[4].Destination = 3;
+            graph.edge[4].Weight = 4;
+
+            KruskalAlgorithm.Kruskal(graph);
             
             
         }
